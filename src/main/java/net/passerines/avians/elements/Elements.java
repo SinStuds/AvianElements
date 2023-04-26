cdd969436887d7182f9c672b68435ed32a1aef29
AnonymousBluejay <anonymousjayblue@gmail.com>
2023-04-25 20:30:59 -0700

    Added Elements

---
+++ src/main/java/net/passerines/avians/elements/Elements.java
@@ -0,0 +1,40 @@
+package net.passerines.avians.elements;
+
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.TextColor;
+import net.passerines.avians.util.Chat;
+
+public class Elements {
+
+    //Fire and sub elements
+    public static final Element FIRE = new Element("fire", "Fire", TextColor.color(255, 128, 0));
+    public static final Element FIRE_SULFUREOUS = new Element("fire_sulfureous", "Sulfureous Fire", TextColor.color(255, 255, 0));
+    public static final Element FIRE_UNYIELDING = new Element("fire_unyielding", "Unyielding Inferno", TextColor.color(255, 0, 0));
+    public static final Element FIRE_AZURE = new Element("fire_azure", "Azure Flames", TextColor.color(51, 255, 255));
+
+    private static class Element {
+        private final String id;
+        private final String name;
+        private final TextColor color;
+        private final Component displayName;
+        private Element(String id, String name, TextColor color) {
+            this.id = id;
+            this.name = name;
+            this.color = color;
+            displayName = Chat.formatC(name).color(color);
+        }
+
+        public String getId() {
+            return id;
+        }
+        public String getName() {
+            return name;
+        }
+        public TextColor getColor() {
+            return color;
+        }
+        public Component getDisplayName() {
+            return displayName;
+        }
+    }
+}
